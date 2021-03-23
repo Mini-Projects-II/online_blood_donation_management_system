d195cad74e69d6f8e6fc8fa01f846cd549fce422
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-03-23 18:56:58 +0530

    update

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import './../CSS/Navigation.css';
 export default function Navigation() {
     return (
-        <div>
-            
+       <>
+        <div id = "nav">
+            <ul>
+                <li> <a href = "#" >About Us</a></li>
+                <li><a href = "#" >About Us</a></li>
+                <li><a href = "#" >About Us</a></li>
+                <li><a href = "#" >About Us</a></li>
+            </ul>
         </div>
+       </>
     )
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Banner_1 from './../image/Banner_1.jpg';
+import Navigation from './../Components/Navigation';
 
 
 export default function HomePage() {
     return (
+        <>
+        <Navigation/>
         <div className = "Banner">
             <img src = {Banner_1} alt = "Banner_1"></img>
         </div>
+        </>
     )
 }
