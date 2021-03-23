1b736bcf64037f2819214ee466a51aef20272d1e
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-03-23 08:26:51 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
-
+import './CSS/HomePage.css';
+import HomePage from "./Pages/HomePage.js";
 function App() {
   return (
     <div className="App">
-      
+      <HomePage/>
     </div>
   );
 }
---
+++ src/Pages/HomePage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Banner_1 from './../image/Banner_1.jpg';
+
+
+export default function HomePage() {
+    return (
+        <div className = "Banner">
+            <img src = {Banner_1} alt = "Banner_1"></img>
+        </div>
+    )
+}
