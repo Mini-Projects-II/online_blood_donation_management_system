b8601d5e226e58ce0b426cb6c1038405c5fc490d
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-03-24 18:08:08 +0530

    creating drop down menu

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import './CSS/HomePage.css';
-import './Pages/HomePage';
+import HomePage from './Pages/HomePage';
 import LoginAsHospital from './Pages/LoginAsHospital';
 import LoginAsPatient from './Pages/LoginAsPatient'
 import LoginAsDonor from './Pages/LoginAsDonor'
@@ -8,7 +8,7 @@ import LoginAsDonor from './Pages/LoginAsDonor'
 function App() {
   return (
     <div className="App">
-      <LoginAsDonor />
+      <HomePage />
     </div>
   );
 }
--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -5,10 +5,22 @@ export default function Navigation() {
        <>
         <div id = "nav">
             <ul>
-                <li> <a href = "#" >About Us</a></li>
+                <li> <a href = "#" >Login</a>
+                    <ul id = "subm-1">
+                        <li> <a href = "#">Login As Donor </a></li>
+                        <li> <a href = "#">Login As Patient </a></li>
+                        <li> <a href = "#">Login As Hospital</a></li>
+                    </ul>
+                </li>
+                <li><a href = "#" >Signup</a>
+                    <ul id= "subm-2">
+                        <li> <a href = "#">Signup As Donor </a></li>
+                        <li> <a href = "#">Signup As Patient </a></li>
+                        <li> <a href = "#">Signup As Hospital</a></li>
+                    </ul>
+                </li>
                 <li><a href = "#" >About Us</a></li>
-                <li><a href = "#" >About Us</a></li>
-                <li><a href = "#" >About Us</a></li>
+                <li><a href = "#" >Contact Us</a></li>
             </ul>
         </div>
        </>
