df277b6ec21c5f96952cdb7e1744df06aea6968e
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2021-03-26 17:49:52 +0530

    update css

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -5,12 +5,15 @@ import naveen from './../image/Naveen.jpg'
 import ram from './../image/Ram.jpg'
 import vineet from './../image/Vineet.jpg'
 import './../CSS/AboutUs.css'
+import Navigation from './../Components/Navigation'
 import AboutUsComponent from '../Components/AboutUsComponent'
 
 export default function AboutUs() {
     return (
         <div>
-            <h1>About US</h1>
+            <Navigation/>
+            <hr></hr>
+            <h1>About US</h1><hr></hr>
             <AboutUsComponent imageName={ayush} name="Ayush Goyal" email="ayush.goyal_cs18@gla.ac.in" mob="8265881134" add="Goverdhan, Mathura"/>
             <AboutUsComponent imageName={gopal} name="Gopal Tiwari" email="gopal.tiwari_cs18@gla.ac.in" mob="7060455295" add="Goverdhan, Mathura"/>
             <AboutUsComponent imageName={naveen} name="Naveen Gupta" email="naveen.gupta_cs18@gla.ac.in" mob="6397363206" add="Chhata, Mathura"/>
