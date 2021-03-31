68f60564ad76e0ee4c3b4900c2e75f89202866d4
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-03-31 23:43:43 +0530

    adding validation

--- src/Pages/SignupAsDonor.js
+++ src/Pages/SignupAsDonor.js
@@ -3,51 +3,41 @@ import backImage from '../Components/backgroundCSSForSeprateDiv'
 import './../CSS/SignupPage.css'
 import Navigation from './../Components/Navigation';
 export default function SignupAsDonor() {
-    const [donor, setDonor] = useState({pname:"",parentsName:"",address:"",mobileNumber:"",gender:"",bloodGroup:"",password:""});    
-    const updateName=(event)=>{
-        event.preventDefault();
-        setDonor({...donor,pname:event.target.value});
-    }
+    const [newRecord, setNewRecord] = useState({name:"",parents_name:"",address:"",mobile_number:"",gender:"null",bloodgroup:"null",password:""}); 
+    const [record, setRecord] = useState([]);
+    const handleInput = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setNewRecord({...newRecord, [name]:value});
 
-    const updateparentsName=(event)=>{
-        setDonor({...donor,parentsName:event.target.value});
     }
-
-    const updateaddress=(event)=>{
-        setDonor({...donor,address:event.target.value});
-    }
-
-    const updatemobileNumber=(event)=>{
-        setDonor({...donor,mobileNumber:event.target.value});
-    }
-
-    const updategender=(event)=>{
-        setDonor({...donor,gender:event.target.value});
-    }
-
-    const updatebloodGroup=(event)=>{
-        setDonor({...donor,bloodGroup:event.target.value});
-    }
-
-    const updatepassword=(event)=>{
-        setDonor({...donor,password:event.target.value});
-        if(donor.pname !== "" && donor.parentsName !== "" && donor.address !== "" && donor.mobileNumber !== "" && donor.gender !== "" && donor.bloodGroup !== "" && donor.password !== "" && donor.goi_Id !== ""){
-            document.getElementById("submit").style.backgroundColor = "red";
-            document.getElementById("submit").disabled = false;
+    const handleRecord = (e) =>{
+        e.preventDefault();
+        if(!(newRecord.name && newRecord.address && newRecord.mobile_number && newRecord.parents_name && newRecord.password)){
+            alert("Please Fill all Data in form");
+        }
+        else if( newRecord.bloodgroup === "null" && newRecord.gender === "null"){
+            alert("Please select valid field");
+        }
+        else if( newRecord.mobile_number.length != 10){
+            alert("Please enter valid mobile no ");
+            setNewRecord({...newRecord, mobile_number:""});
+        }
+        else if( newRecord.password.length <8){
+            alert("Please enter password of length minimum 8");
+            setNewRecord({...newRecord, password:""});
         }
         else{
-            document.getElementById("submit").style.backgroundColor = "darkgray";
-            document.getElementById("submit").disabled = true;
+        setRecord([...record, newRecord]);
+        setNewRecord({name:"",parents_name:"",address:"",mobile_number:"",gender:"null",bloodgroup:"null",password:""});
         }
-    }
-
-    const updategoi_Id=(event)=>{
-        setDonor({...donor,goi_Id:event.target.value});
-    }
-
-    function checkDetails(){
 
     }
+
+    
+
+  
+    
     return (
         <>
         <Navigation/>
@@ -56,48 +46,48 @@ export default function SignupAsDonor() {
                 <div className="signup-header">
                     <h2>Sign Up</h2>
                 </div>
-                <form>
+                <form onSubmit = {handleRecord}>
                 <div className="signup-container">
-                    <label for="name">Donor Name</label>
-                    <input type="text" id="name" name="name" placeholder="Enter Donor Name" onChange={updateName}/><br></br><br></br>
-                    <label for="parents-name">Mother's/Father's Name</label><br></br>
-                    <input type="text" id="parents-name" name="parents-name" placeholder="Enter Mother's/Father's Name" onChange={updateparentsName}/><br></br><br></br>
-                    <label for="address">Current Residential's Address</label><br></br>
-                    <input type="text" id="address" name="address" placeholder="Enter Current Residential's Address" onChange={updateaddress}/><br></br><br></br>
-                    <label for="mobile-number">Mobile Number</label><br></br>
-                    <input type="text" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number" onChange={updatemobileNumber}/><br></br><br></br>
-                    <label for="gender">Gender</label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
-                    <label for="bloodgroup">Blood Group</label><br></br>
-                    <select name="gender" for="gender" onChange={updategender}>
-                            <option selected>Select Value</option>
-                            <option>Female</option>
-                            <option>Male</option>
-                            <option>Others</option>
+                    <label htmlFor="name">Donor Name</label>
+                    <input type="text" id="name" name="name" placeholder="Enter Donor Name"  value = {newRecord.name} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="parents_name">Mother's/Father's Name</label><br></br>
+                    <input type="text" id="parents_name" name="parents_name" placeholder="Enter Mother's/Father's Name" value = {newRecord.parents_name} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="address">Current Residential's Address</label><br></br>
+                    <input type="text" id="address" name="address" placeholder="Enter Current Residential's Address" value = {newRecord.address} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="mobile_number">Mobile Number</label><br></br>
+                    <input type="number" maxLength = "10" id="mobile_number" name="mobile_number" placeholder="Enter Mobile Number" value = {newRecord.mobile_number} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="gender">Gender</label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
+                    <label htmlFor="bloodgroup">Blood Group</label><br></br>
+                    <select name="gender" htmlFor="gender"  value = {newRecord.gender} onChange={handleInput}>
+                            <option value = "null" selected>Select Gender</option>
+                            <option value= "Female">Female</option>
+                            <option value="Male">Male</option>
+                            <option value="Others">Others</option>
                     </select>&nbsp;&nbsp; 
-                    <select name="bloodgroup" for="bloodgroup" onChange={updatebloodGroup}>
-                            <option selected>Select Value</option>
-                            <option>A+</option>
-                            <option>A-</option>
-                            <option>B+</option>
-                            <option>B-</option>
-                            <option>AB+</option>
-                            <option>AB-</option>
-                            <option>O+</option>
-                            <option>O-</option>
+                    <select name="bloodgroup" htmlFor="bloodgroup" value = {newRecord.bloodgroup} onChange={handleInput}>
+                        <option value = "null" selected> Select Blood Group</option>
+                            <option value="A+" >A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
                     </select><br></br><br></br>
-                    <label for="goi-Id">GOI ID</label><br></br>
-                    <select name="goi_Id" for="goi_Id" onChange={updategoi_Id}>
-                            <option selected>Select Value</option>
-                            <option>Aadhar Number</option>
-                            <option>PAN</option>
-                            <option>Driving Liscense</option>
-                            <option>Voter Id</option>
-                            <option>Passport Number</option>
-                    </select><br></br><br></br>
-                    <label for="password">Password</label><br></br>
-                    <input type="text" id="password" name="password" placeholder="Enter Password" onChange={updatepassword}/><br></br><br></br>
+                    {/*<label htmlFor="goi_Id">GOI ID</label><br></br>
+                    <select name="goi_Id" htmlFor="goi_Id" onChange={handleInput}>
+                            <option value={newRecord.} selected>Select Value</option>
+                            <option value={newRecord.}>Aadhar Number</option>
+                            <option value={newRecord.}>PAN</option>
+                            <option value={newRecord.}>Driving Liscense</option>
+                            <option value={newRecord.}>Voter Id</option>
+                            <option value={newRecord.}>Passport Number</option>
+    </select><br></br><br></br>*/}
+                    <label htmlFor="password">Password</label><br></br>
+                    <input type="password" id="password" name="password" placeholder="Enter Password" value = {newRecord.password} onChange={handleInput}/><br></br><br></br>
                 </div>
-                <button type="submit" id="submit" disabled>Submit</button>
+                <button type="submit" id="submit">Submit</button>
                 </form>
             </div>
         </>
--- src/Pages/SignupAsPatient.js
+++ src/Pages/SignupAsPatient.js
@@ -1,22 +1,44 @@
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 import backImage from '../Components/backgroundCSSForSeprateDiv'
 import './../CSS/SignupPage.css'
 import Navigation from './../Components/Navigation';
+
 export default function SignupAsPatient() {
-    var btn = document.getElementById("submit");
-    const checkDetails=()=>{
-        var pname = document.getElementById("name").value;
-        var parentsName = document.getElementById("parents-name").value;
-        var address = document.getElementById("address").value;
-        var mobileNumber = document.getElementById("mobile-number").value;
-        var gender = document.getElementById("gender").value;
-        var bloodGroup= document.getElementById("bloodgroup").value;
-        var password = document.getElementById("password").value;
-        if(pname === "" || parentsName === "" || address === "" || mobileNumber === "" || gender ==="" || bloodGroup === "" || password === ""){
-            alert("No");
-            btn.disabled = true;
-        }
+    const [newRecord, setNewRecord] = useState({name:"",parents_name:"",address:"",mobile_number:"",gender:"null",bloodgroup:"null",password:""}); 
+    const [record, setRecord] = useState([]);
+    const handleInput = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        setNewRecord({...newRecord, [name]:value});
+
     }
+    const handleRecord = (e) =>{
+        e.preventDefault();
+        if(!(newRecord.name && newRecord.address && newRecord.mobile_number && newRecord.parents_name && newRecord.password)){
+            alert("Please Fill all Data in form");
+        }
+        else if( newRecord.bloodgroup === "null" && newRecord.gender === "null"){
+            alert("Please select valid field");
+        }
+        else if( newRecord.mobile_number.length != 10){
+            alert("Please enter valid mobile no ");
+            setNewRecord({...newRecord, mobile_number:""});
+        }
+        else if( newRecord.password.length <8){
+            alert("Please enter password of length minimum 8");
+            setNewRecord({...newRecord, password:""});
+        }
+        else{
+        setRecord([...record, newRecord]);
+        setNewRecord({name:"",parents_name:"",address:"",mobile_number:"",gender:"null",bloodgroup:"null",password:""});
+        }
+
+    }
+
+    
+
+  
+    
     return (
         <>
         <Navigation/>
@@ -25,37 +47,48 @@ export default function SignupAsPatient() {
                 <div className="signup-header">
                     <h2>Sign Up</h2>
                 </div>
-                <form>
+                <form onSubmit = {handleRecord}>
                 <div className="signup-container">
-                    <label for="name">Patient Name</label>
-                    <input type="text" id="name" name="name" placeholder="Enter Patient Name"/><br></br><br></br>
-                    <label for="parents-name">Mother's/Father's Name</label><br></br>
-                    <input type="text" id="parents-name" name="parents-name" placeholder="Enter Mother's/Father's Name"/><br></br><br></br>
-                    <label for="address">Current Residential's Address</label><br></br>
-                    <input type="text" id="address" name="address" placeholder="Enter Current Residential's Address"/><br></br><br></br>
-                    <label for="mobile-number">Mobile Number</label><br></br>
-                    <input type="text" id="mobile-number" name="mobile-number" placeholder="Enter Mobile Number"/><br></br><br></br>
-                    <label for="gender">Gender</label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
-                    <label for="bloodgroup">Blood Group</label><br></br>
-                    <select name="gender" for="gender">
-                            <option>Female</option>
-                            <option>Male</option>
-                            <option>Others</option>
-                    </select> &nbsp;&nbsp;
-                    <select name="bloodgroup" for="bloodgroup">
-                            <option>A+</option>
-                            <option>A-</option>
-                            <option>B+</option>
-                            <option>B-</option>
-                            <option>AB+</option>
-                            <option>AB-</option>
-                            <option>O+</option>
-                            <option>O-</option>
+                    <label htmlFor="name">Patient Name</label>
+                    <input type="text" id="name" name="name" placeholder="Enter Patient Name"  value = {newRecord.name} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="parents_name">Mother's/Father's Name</label><br></br>
+                    <input type="text" id="parents_name" name="parents_name" placeholder="Enter Mother's/Father's Name" value = {newRecord.parents_name} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="address">Current Residential's Address</label><br></br>
+                    <input type="text" id="address" name="address" placeholder="Enter Current Residential's Address" value = {newRecord.address} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="mobile_number">Mobile Number</label><br></br>
+                    <input type="number" maxLength = "10" id="mobile_number" name="mobile_number" placeholder="Enter Mobile Number" value = {newRecord.mobile_number} onChange={handleInput}/><br></br><br></br>
+                    <label htmlFor="gender">Gender</label>&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&nbsp;&nbsp;
+                    <label htmlFor="bloodgroup">Blood Group</label><br></br>
+                    <select name="gender" htmlFor="gender"  value = {newRecord.gender} onChange={handleInput}>
+                            <option value = "null" selected>Select Gender</option>
+                            <option value= "Female">Female</option>
+                            <option value="Male">Male</option>
+                            <option value="Others">Others</option>
+                    </select>&nbsp;&nbsp; 
+                    <select name="bloodgroup" htmlFor="bloodgroup" value = {newRecord.bloodgroup} onChange={handleInput}>
+                        <option value = "null" selected> Select Blood Group</option>
+                            <option value="A+" >A+</option>
+                            <option value="A-">A-</option>
+                            <option value="B+">B+</option>
+                            <option value="B-">B-</option>
+                            <option value="AB+">AB+</option>
+                            <option value="AB-">AB-</option>
+                            <option value="O+">O+</option>
+                            <option value="O-">O-</option>
                     </select><br></br><br></br>
-                    <label for="password">Password</label><br></br>
-                    <input type="text" id="password" name="password" placeholder="Enter Password"/><br></br><br></br>
+                    {/*<label htmlFor="goi_Id">GOI ID</label><br></br>
+                    <select name="goi_Id" htmlFor="goi_Id" onChange={handleInput}>
+                            <option value={newRecord.} selected>Select Value</option>
+                            <option value={newRecord.}>Aadhar Number</option>
+                            <option value={newRecord.}>PAN</option>
+                            <option value={newRecord.}>Driving Liscense</option>
+                            <option value={newRecord.}>Voter Id</option>
+                            <option value={newRecord.}>Passport Number<option>
+    </select><br></br><br></br>*/}
+                    <label htmlFor="password">Password</label><br></br>
+                    <input type="password" id="password" name="password" placeholder="Enter Password" value = {newRecord.password} onChange={handleInput}/><br></br><br></br>
                 </div>
-                <button type="submit" id="submit" onClick={checkDetails}>Submit</button>
+                <button type="submit" id="submit">Submit</button>
                 </form>
             </div>
         </>
