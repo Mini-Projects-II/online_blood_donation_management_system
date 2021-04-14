b3f749bb0189c7f3a9523d93cc4fab6f4df32067
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-04-14 15:32:08 +0530

    upadate

--- src/Pages/PatientDashBoard.js
+++ src/Pages/PatientDashBoard.js
@@ -9,21 +9,38 @@ export default function PatientDashBoard() {
                 <DashBoardNav icon = "fas fa-user" user= "Welcome Patient Name"  val1="Request Blood" val2 = "Request Instruction" val3="History" val4="Logout"/>
                 <h1 className ="p_hading">Patient Requests</h1>
                 <div className="p_request">
-                    <div className = "req1">
+                <div className = "req">
                         <div className = "r_details">
-                        <p className = "p_detail">Req id:</p>
-                        <p className = "p_detail"> Date</p>
-                        <p className = "p_detail">Time</p>
-                        <p className = "p_detail">Hospital Name</p>
-                        <p className = "p_detail">Hospital Address</p>
-                        <p className = "p_detail"></p>
+                        <p className = "p_detail_1">Req id:</p>
+                        <p className = "p_detail_2"> Date:</p>
+                        <p className = "p_detail_3">Time:</p>
+                        <p className = "p_detail_4">Hospital Name:</p>
+                        <p className = "p_detail_5">Hospital Address:</p>
+                        <p className = "p_detail_6">Room No:</p>
                         </div>
                         <div className = "r_status">
-                            <p>Status:Pending</p>
-                            <button>Delete Request</button>
+                            <p className = "p_status"> Status:Pending</p>
+                            <button className = "p_button">Cancel Request</button>
                         </div>
 
                     </div>
+                    
+                    <div className = "req">
+                        <div className = "r_details">
+                        <p className = "p_detail_1">Req id:</p>
+                        <p className = "p_detail_2"> Date:</p>
+                        <p className = "p_detail_3">Time:</p>
+                        <p className = "p_detail_4">Hospital Name:</p>
+                        <p className = "p_detail_5">Hospital Address:</p>
+                        <p className = "p_detail_6">Room No:</p>
+                        </div>
+                        <div className = "r_status">
+                            <p className = "p_status"> Status:Pending</p>
+                            <button className = "p_button">Cancel Request</button>
+                        </div>
+
+                    </div>
+                    
 
                 </div>
             </>
