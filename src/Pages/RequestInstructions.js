e35a8b5812fd4082b697b39dd61798611e433c5b
GopalTiwari7 <gopal.tiwari_cs18@gla.ac.in>
2021-04-20 00:05:31 +0530

    end commit

--- src/Pages/RequestInstructions.js
+++ src/Pages/RequestInstructions.js
@@ -9,20 +9,17 @@ export default function RequestInstructions() {
         <div>
             <DashBoardNav  icon = "fas fa-user" val1="Donate Blood" val2 = "Request Instruction" val3="History" val4="Logout"/>
             {/* <br/><br/><br/><br/><br/> */}
-            <h1>Request Instruction</h1>
+            <h1>Request Instruction</h1><hr></hr>
             <div className = "Banner">
             <img src = {Banner_3} alt = "Banner_3" ></img>
             </div>
             <div id="content">
-                    <h2>Risks for a person receiving blood can be divided into several categories :</h2><br/>
+                    <h2>Risks for a person receiving blood can be divided into several categories (Precautions) :</h2><br/>
                     <ol start="1" id="list1">
-                    <li><b>Blood Type Matching : </b>Blood type should be compatible</li><br/>
+                    <li><b>Blood Type Matching : </b>Blood type should be compatible.</li><br/>
                     <li><b>Allergic and Other Reactions : </b>Delayed hemolytic reactions are usually mild and are caused by certain chemicals in the donor’s blood</li><br></br>
                     <li><b>Infection : </b>Infectious complications receive the most attention, especially since the HIV epidemic.</li><br></br>
-                    <li>Hemoglobin level- A minimum of 12.5 g/dL.</li><br></br>
-                    <li>Blood pressure- Diastolic: 50–100 mm Hg, Systolic: 100–180 mm Hg.</li><br></br>
-                    <li>Body temperature- Should be normal, with an oral temperature not exceeding 37.5 °C.</li><br/>
-                    <li>The time period between successive blood donations should be more than 3 months.</li>
+                    <li><b>Banking Your Own Blood : </b>An individual may bank his/her own blood for a transfusion during an upcoming surgical procedure.</li><br></br>
                 </ol>
             </div>
         </div>
