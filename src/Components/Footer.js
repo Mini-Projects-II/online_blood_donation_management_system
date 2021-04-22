4a7a4a6113298aaa55bd3b50288432fe025e0bd4
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-04-22 22:47:31 +0530

    expected final commit

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -10,11 +10,11 @@ export default function Footer() {
                 <p >The Online Blood Donation management System is to create an e-Information about the donor and organization that are related to donating the blood. Through this application any person who is interested in donating the blood can register himself in the same way if any organization wants to register itself with this site that can also register. Moreover if any general consumer wants to make request blood online he can also take the help of this site. Admin is the main authority who can do addition, deletion, and modification if required.</p>
                 </div>
                 
-                <div id = "soc">
+               {/*} <div id = "soc">
                     <i class="fa fa-facebook" aria-hidden="true"></i>
                     <i class="fa fa-twitter" aria-hidden="true"></i>
                     <i class="fa fa-instagram" aria-hidden="true"></i>  
-                </div>
+    </div>*/}
                 
             </div>
             <div className = "contact">
